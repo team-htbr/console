6b6c7252c417f3584b81a18620f05d906d94a08e
rubenmartin.thys <rubenmartin.thys@student.odisee.be>
2017-03-25 13:42:29 +0100

    add send to topic functionality

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -9,16 +9,17 @@
     // 3. Toggle 'Android monitoring at the bottom of Android Studio and press the 'SHOW TOKEN' button
     // 4. Copy string after 'Token: ' 
     // 5. Paste token in constant 'phoneKey'
-    const phoneKey = '';
-    const jsonString = { "notification": { "title": "Bloeddonatie", "body": "Notification for specific device" }, "to" : phoneKey };
+    const phoneKey = 'dE2Z0lNiB_w:APA91bG4pyDqWhzJKBQjL1z7gPxlcIp8tYBtCvSdtBWFY6ByBF42537J2a6iypJ67T0KRkdGLgJ_u0l7Su2VuE75BVXPU76_fshdUJQ2C5IPkp6dcje_q7vL4QLkc8xc0S0ykBR3q7yx';
+    const msgSpecific = { "notification": { "title": "Rode Kruis", "body": "Notification for specific device" }, "to" : phoneKey };
+    const msgTopic = { "notification": { "title": "Rode Kruis", "body": "Notification for devices subscribed to a topic" }, "to" : "/topics/blood-AB" };
 
-    var sendNotification = function() {
+    var sendNotification = function(msg) {
         $.ajax({
             headers: { 'Authorization': serverKey },
             type: 'POST',
             url : 'https://fcm.googleapis.com/fcm/send',
             contentType : 'application/json',
-            data : JSON.stringify(jsonString),
+            data : JSON.stringify(msg),
             dataType : 'json',
             succes : function(data) {
                 console.log(data);
@@ -27,8 +28,14 @@
     };
 
     $(document).ready(function() {
-        $('#button').on('click', function() {
-            sendNotification()
+        $('#btnSpecific').on('click', function() {
+            sendNotification(msgSpecific)
+        });
+    });
+
+    $(document).ready(function() {
+        $('#btnTopic').on('click', function() {
+            sendNotification(msgTopic)
         });
     });
 
