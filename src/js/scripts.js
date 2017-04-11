28b7b6e3995b55285fb6164c38bc2caac582309e
Ruben Thys <ruben.thys@student.odisee.be>
2017-04-11 11:51:41 +0200

    clean up code

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -10,8 +10,8 @@
     // 4. Copy string after 'Token: ' 
     // 5. Paste token in constant 'phoneKey'
     const phoneKey = 'dE2Z0lNiB_w:APA91bG4pyDqWhzJKBQjL1z7gPxlcIp8tYBtCvSdtBWFY6ByBF42537J2a6iypJ67T0KRkdGLgJ_u0l7Su2VuE75BVXPU76_fshdUJQ2C5IPkp6dcje_q7vL4QLkc8xc0S0ykBR3q7yx';
-    const msgSpecific = { "notification": { "title": "Rode Kruis", "body": "Notification for specific device" }, "to" : phoneKey };
-    const msgTopic = { "notification": { "title": "Rode Kruis", "body": "Notification for devices subscribed to a topic" }, "to" : "/topics/blood-AB" };
+    const msgSpecific = { 'notification': { 'title': 'Rode Kruis', 'body': 'Notification for specific device' }, 'to' : phoneKey };
+    const msgTopic = { 'notification': { 'title': 'Rode Kruis', 'body': 'Notification for devices subscribed to a topic' }, 'to' : '/topics/blood-AB' };
 
     var sendNotification = function(msg) {
         $.ajax({
