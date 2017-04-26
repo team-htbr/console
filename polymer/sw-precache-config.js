56f32fe7a9ad21fb3d0031582ac01ec028a4aae9
Hanne Goossens <hanne.goossens@student.odisee.be>
2017-04-26 13:09:28 +0200

    polymer 1

--- polymer/sw-precache-config.js
+++ polymer/sw-precache-config.js
@@ -14,7 +14,7 @@ module.exports = {
   staticFileGlobs: [
     '/index.html',
     '/manifest.json',
-    '/bower_components/webcomponentsjs/*',
+    '/bower_components/webcomponentsjs/webcomponents-lite.min.js',
   ],
   navigateFallback: 'index.html',
 };
